238e03919cb53cadbe980f8badd62ef99dfcf24b
Katricia Herring <kherring@nd.edu>
2018-02-27 06:08:46 -0500

    prop loop

--- initSale.sh
+++ initSale.sh
@@ -4,9 +4,9 @@
 
 tables=(Customer Manufacturer Product Salesperson Sale)
 shopt -s nullglob
-for s in $tables; do
+for s in ${tables[@]}; do
+  # echo $s
   sqlplus kherring/msa2012 @create$s << HERE
   exit
 HERE
-
 done
