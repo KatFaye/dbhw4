8bbcafce5e220f38a7b8439ca89f59b482a05006
Katricia Herring <kherring@nd.edu>
2018-02-27 06:05:46 -0500

    $

--- initSale.sh
+++ initSale.sh
@@ -2,10 +2,10 @@
 
 # ssh kherring
 
-tables=(customer manufacturer product salesperson sale)
+tables=(Customer Manufacturer Product Salesperson Sale)
 shopt -s nullglob
-for s in tables; do
-  sqlplus kherring/msa2012 @$s << HERE
+for s in $tables; do
+  sqlplus kherring/msa2012 @create$s << HERE
   exit
 HERE
 
